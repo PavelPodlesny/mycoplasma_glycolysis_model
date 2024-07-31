107de62b5ff0fe640f5fee5668267937c1983919
Pavel Podlesnyi <99507048+PavelPodlesny@users.noreply.github.com>
2024-07-31 16:55:15 +0300

    31/7/24

--- source/fitModelToData.m
+++ source/fitModelToData.m
@@ -10,6 +10,7 @@ function fitModelToData(model_dir, data_dir, nva)
                 nva.sens_analysis logical = false
                 nva.pooled        logical = true % pool data together (a model will be fitted across all experimental samples)
                 nva.error_model      char = 'constant'
+                nva.SPECIES_UNITS    char = 'micromolarity'
         end
 
         cleanupObj = onCleanup(@cleanMeUp);
@@ -32,82 +33,73 @@ function fitModelToData(model_dir, data_dir, nva)
                 models_list = dir([model_path filesep '*.xml']);
                 model = sbmlimport([model_path filesep models_list(1).name]);
                 sbioaccelerate(model, getconfigset(model, 'default'));
+                
                 %% create output directory and read experimental data
                 [output_dir, date] = createOutputDirectory(data_path);
 
-                %% log
+                %% write log file
                 nva.model_path = model_path;
                 nva.data_path = data_path;
                 nva.output_path = output_dir;
                 nva.date = date;
                 logger(nva);
-                %%
                 
+                %% load model's variants (different initial conditions: 1 variant per 1 group)
                 variants = getModelVariants(data_path);
-                % a table with experimental data
-                % exp_data.Properties.VariableNames = {'VariantID', 'GroupID', 'Time', ... Species ...}
-                % groups within one variant are pooled together,
-                % so variant == experimental condition, groups == technical repeats
-                % VariantID == "default" means don't apply any variants and use original model's parameters
+                
+                %% load a table with experimental data
+                %  exp_data.Properties.VariableNames = {'GroupID', 'Time', ... Species ...}
                 exp_data = readExperimentalData(data_path);
-
-                %nva.repeats_number*length(params2est)
-                % calculate size of a output table with estimated parameters
+                assert(length(variants)==length(unique(exp_data.GroupID)), ...
+                        "ERROR: number of variants does not match number of groups in exp. data!");
+                %% get experimental data
+                [grp_data, params2est, resp_map] = getFitData(data_path, exp_data, nva.SPECIES_UNITS);
                 %% create a table where optimization's results will be saved
-                opt_res_vars = {'Variant', 'Iteration', 'Name', 'Estimate', 'StandardError', 'Bounds'};
-                opt_res_table = table('Size', [0, 6], ...
-                                        'VariableTypes', {'string', 'int32', 'string', 'double', 'double', 'double'}, ...
+                opt_res_vars = {'Iteration', 'Name', 'Estimate', 'StandardError', 'Bounds'};
+                opt_res_table = table('Size', [0, 5], ...
+                                        'VariableTypes', {'int32', 'string', 'double', 'double', 'double'}, ...
                                         'VariableNames', opt_res_vars);
                                         
                 disp('Start optimization...');
-                for (i=1:width(variants))
-                        var = variants(i);
-                        [grp_data, params2est, resp_map] = getFitData(data_path, exp_data, var.Name, model.Species);
-                        %% define path for graphs
-                        plot_path = [output_dir filesep 'fit_exp_plots_' var.Name '.pdf'];
-                        page_is_1st = true; % flag, which indicates, that a graph on the 1st page will be plotted 
-                        disp(['Fit exp. data for condition: ' var.Name]);
-                        %% optimization                        
-                        for (iter=1:nva.repeats_number)
-                                disp(['   ' datestr(now, 'dd-mmm-HH-MM') ': iter. ' num2str(iter) ' starts...']);
-                                tic;
-                                optim_results = sbiofit(model, grp_data, resp_map, params2est, [], 'scattersearch', ...
-                                                        'Variants', var, 'UseParallel', nva.use_parallel, ...
-                                                        'Pooled', nva.pooled, 'ErrorModel', nva.error_model, ...
-                                                        'SensitivityAnalysis', nva.sens_analysis);
-                                elapsed_time = toc;
-                                disp(['      iter. ' num2str(iter) ' finished: elapsed time = ' num2str(elapsed_time) ' sec.']);
-                                % update a table with optimization's results
-                                [sim_data, est_params] = fitted(optim_results);
-                                %% plot graphs
-                                plotSimData(sim_data, plot_path, page_is_1st,...
-                                        plot_exp_data = true, grp_data = grp_data, resp_map = resp_map);
+                %% optimization                        
+                for (iter=1:nva.repeats_number)
+                        page_is_1st = true;
+                        plot_path = [output_dir filesep 'fit_exp_iter_' num2str(iter) '.pdf'];
+                        
+                        disp(['   ' datestr(now, 'dd-mmm-HH-MM') ': iter. ' num2str(iter) ' starts...']);
+                        tic;
+                        optim_results = sbiofit(model, grp_data, resp_map, params2est, [], 'scattersearch', ...
+                                                'Variants', variants, 'UseParallel', nva.use_parallel, ...
+                                                'Pooled', nva.pooled, 'ErrorModel', nva.error_model, ...
+                                                'SensitivityAnalysis', nva.sens_analysis);
+                        elapsed_time = toc;
+                        disp(['      iter. ' num2str(iter) ' finished: elapsed time = ' num2str(elapsed_time) ' sec.']);
+                        
+                        % update a table with optimization's results
+                        [sim_data, est_params] = fitted(optim_results);
+                        % !!! sim_data is an array
+                        %% plot graphs
+                        
+                        for (i=1:width(variants))
+                                var = variants{i};
+                                grp_var_data = exp_data(exp_data.GroupID==str2num(var.Name),:);
+                                plotSimData(sim_data(i), plot_path, page_is_1st,...
+                                        plot_exp_data = true, grp_data = grp_var_data, resp_map = resp_map);
                                 if page_is_1st
                                         page_is_1st = false;
-                                end 
-                                %fig = plotSimData(sim_data, plot_exp_data = true, grp_data = grp_data, resp_map = resp_map);
-                                %plot(optim_results);
-                                %if page_is_1st
-                                %        exportgraphics(fig, plot_path, 'BackgroundColor', 'none', 'ContentType', 'vector');
-                                %        page_is_1st = false;
-                                %else
-                                %        exportgraphics(fig, plot_path, 'Append', true, ...
-                                %                        'BackgroundColor', 'none', 'ContentType', 'vector');
-                                %end 
-                                %close(fig);
-                                %% debug
-                                %disp(est_params);
-                                %disp(varfun(@class, est_params, 'OutputFormat', 'cell'));
-                                %% update the table 
-                                iter_col = repmat(iter, height(est_params), 1);
-                                var_col = repmat(var.Name, height(est_params), 1);
-                                est_params.Iteration = iter_col;
-                                est_params.Variant   = var_col;
-                                opt_res_table = vertcat(opt_res_table, est_params);
-                                save([output_dir filesep 'OptimResults_var_' var.Name '_iter' num2str(iter) '.mat'], '-v7.3', 'optim_results');
+                                end
                         end
-                                
+                        %% debug
+                        %disp(est_params);
+                        %disp(varfun(@class, est_params, 'OutputFormat', 'cell'));
+                        %% update the table 
+                        iter_col = repmat(iter, height(est_params), 1);
+                        est_params.Iteration = iter_col;
+                        opt_res_table = vertcat(opt_res_table, est_params);
+                        save([output_dir filesep 'OptimResults_iter' num2str(iter) '.mat'], '-v7.3', 'optim_results');
                 end
+                                
+                %end
                 
                 %writetable(est_params, [output_dir filesep 'EstimatedParams.tsv'], ...
                 %   'FileType', 'text', ...
@@ -137,7 +129,7 @@ end
 
 %% support function
 
-function [grp_data, params2est, resp_map] = getFitData(data_path, exp_data, variant_id, model_species)
+function [grp_data, params2est, resp_map] = getFitData(data_path, exp_data, species_unit)
         %!!! IMPORTANT AGREEMENT !!!: names of variables in a model and an experiment are the same for convinience,
         % so the response map has only formal sense
         %% create estimatedInfo object
@@ -145,34 +137,19 @@ function [grp_data, params2est, resp_map] = getFitData(data_path, exp_data, vari
                                    'TextType', 'string', 'Delimiter', '\t', ...
                                    'ReadVariableNames', true, 'ReadRowNames', false);
         tbl = readtable([data_path filesep 'Bounds.tsv'], opts);
-        params = (tbl.parameter)';
-        bounds = [tbl.lower_bound, tbl.upper_bound];
+        params = (tbl.Parameter)';
+        bounds = [tbl.LowerBound, tbl.UpperBound];
         params2est = estimatedInfo(params, 'Bounds', bounds);
-        
-        %% create groupedData object and ResponseMap
-        % extract from exp_data rows which relate to variant, specified as 'variant_name', then drop VarID column
-        
-        %units = cellstr(["", "second", (tbl.unit)']); % ID, Time, Variables
-        %opts = detectImportOptions([data_path filesep 'ExperimentalData.tsv'], 'FileType', 'text', ...
-        %                           'TextType', 'string', 'Delimiter', '\t', ...
-        %                           'ReadVariableNames', true, 'ReadRowNames', false);
-        %tbl = readtable([data_path filesep 'ExperimentalData.tsv'], opts);
-        tbl = exp_data(strcmp(exp_data.VariantID, variant_id), 2:end);
-        exp_vars = tbl.Properties.VariableNames(3:end);
-        
-        n = height(model_species);
-        % mod_vars = cell(1, n);
-        units = cell(1, n);
-        for i=1:n
-                % mod_vars{i} = model_species(i).Name;
-                units{i} = model_species(i).Units;
-        end
-        
-        % resp_map = strcat(string(mod_vars), " = ", string(exp_vars));
+
+        %% create Response Map
+        exp_vars = exp_data.Properties.VariableNames(3:end); %skip GroupID and Time variables
         resp_map = strcat(string(exp_vars), " = ", string(exp_vars));
-        grp_data = groupedData(tbl, 'GroupID', 'Time'); %grouped data
-        % !!! BAG: restrict units onlu to exp. data 
-        %grp_data.Properties.VariableUnits = [{'', 'second'}, units];
+
+        %% create Grouped Data object
+        units = repmat({species_unit}, 1, size(exp_vars, 2));
+        grp_data = groupedData(exp_data, 'GroupID', 'Time'); %grouped data
+        grp_data.Properties.VariableUnits = [{'', 'second'}, units];
+        %%
 end
 
 
@@ -186,6 +163,8 @@ function [output_dir, date] = createOutputDirectory(data_path)
 end
 
 function vars = getModelVariants(data_path)
+        %% create Variant object (initial cond.) for each group
+        %  variants are stored as a cell array
         opts = detectImportOptions([data_path filesep 'Variant.tsv'], 'FileType', 'text', ...
                                    'TextType', 'string', 'Delimiter', '\t', ...
                                    'ReadVariableNames', true, 'ReadRowNames', false);
@@ -198,7 +177,8 @@ function vars = getModelVariants(data_path)
                 for j=1:size(var_tbl, 1)
                         var_cell{j} = var_tbl(j,:);
                 end
-                vars(i) = sbiovariant(ids(i), var_cell);
+                % !!! rewrite this part to work with not numeric VariantID
+                vars{i} = sbiovariant(num2str(ids(i)), var_cell);
         end
 end
 
--- source/plotSimData.m
+++ source/plotSimData.m
@@ -5,56 +5,58 @@ function plotSimData(sim_data, plot_path, page_is_1st, nva)
                 plot_path
                 page_is_1st logical
                 nva.plot_exp_data logical = false
-                nva.grp_data % groupedData object
+                nva.grp_data % table
                 nva.resp_map % response map (match between species' names in a model and an experimental data)
         end
 
         % map models' variables to their units and exp. variables
         if nva.plot_exp_data
-                grp_data_table = groupedData2table(nva.grp_data);
-                %% assign values to var2exp dict.
-                var2exp   = dictionary;
+                grp_data_table = nva.grp_data;
+                %% assign values to var2exp dict. (matching between species names in a model and in data)
+                var2exp = dictionary;
                 for i = 1:length(nva.resp_map)
                         parts = split(nva.resp_map(i), ' = ');
                         % parts{1} -- species' name in a model -- key
                         % parts{2} -- species' name in a experiment -- value
                         var2exp(parts{1}) = parts{2};
                 end
-                %%
         end
+        
         %% assign values to var2units dict.
         var2units = dictionary;
-        for i = 1:length(sim_data(1).DataInfo)
-                var2units(sim_data(1).DataInfo{i}.Name) = sim_data(1).DataInfo{i}.Units;
+        for i = 1:length(sim_data.DataInfo)
+                var2units(sim_data.DataInfo{i}.Name) = sim_data.DataInfo{i}.Units;
         end
-        %% plot graphs
         
-        [time, data, names] = getdata(sim_data(1));
-        pages_num = floor(numel(names)/4) + sign(mod(numel(names), 4));
+        %% plot graphs
+        [time, data, names] = getdata(sim_data);
+        pages_num = floor(numel(names)/4) + sign(mod(numel(names), 4)); % 4 plots per page
         for(page = 1:pages_num)
                 fig = figure('Visible','off');
                 for(shift = 1:4)
-                        i = min((page-1)*4 + shift, numel(names));
-                        subplot(2, 2, shift);
-                        plot(time, data(:,i), '-', 'lineWidth', 1);
-                
-                        if (nva.plot_exp_data && isKey(var2exp, names(i)))
-                                var_name = var2exp(names(i));
-                                exp_data = grp_data_table(:, ['GroupID', 'Time', var_name]);
-                                hold on;
-                                s = scatter(exp_data, 'Time', var_name, 'ColorVariable', 'GroupID');
-                                s.SizeData = 10;
-                                hold off;
+                        i = (page-1)*4 + shift;
+                        %i = min((page-1)*4 + shift, numel(names));
+                        if ( i <= numel(names) )
+                                subplot(2, 2, shift);
+                                plot(time, data(:,i), '-', 'lineWidth', 1);
+                                
+                                if (nva.plot_exp_data && isKey(var2exp, names{i}))
+                                        var_name = var2exp(names{i});
+                                        hold on;
+                                        scatter(grp_data_table.Time, grp_data_table{:,[var_name]}, 12, "red");
+                                        hold off;
+                                end
+                                title(names{i}, 'FontSize', 8);
+                                ylabel(['Amount, ' var2units(names{i})], 'Interpreter', 'none', 'FontSize', 8);
+                                xlabel('Time, sec.', 'FontSize', 8);
                         end
-                        title(names(i), 'FontSize', 8);
-                        ylabel(['Amount, ' var2units(names(i))], 'Interpreter', 'none', 'FontSize', 8);
-                        xlabel('Time, sec.', 'FontSize', 8);
                 end
                 if page_is_1st
                         exportgraphics(fig, plot_path, 'BackgroundColor', 'none', 'ContentType', 'vector');
                         page_is_1st = false;
                 else
-                        exportgraphics(fig, plot_path, 'Append', true, 'BackgroundColor', 'none', 'ContentType', 'vector');
+                        exportgraphics(fig, plot_path, 'Append', true, 'BackgroundColor', ...
+                                        'none', 'ContentType', 'vector');
                 end 
                 close(fig);
         end
